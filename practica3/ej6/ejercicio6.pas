408c9637f5dedcd77c31d10887e280d34d760671
Valentin Capelli <valentin.capelli@hotmail.com>
2024-04-30 19:01:40 -0300

    Ejercicio 6

--- practica3/ej6/ejercicio6.pas
+++ practica3/ej6/ejercicio6.pas
@@ -28,28 +28,80 @@ type
 	registroDetalle = record
 		cod_prenda:integer;
 	end;
-	detalle = file of regsitroDetalle;
+	detalle = file of registroDetalle;
+	
+	procedure leerMaestro(var regm:registroMaestro);
+	begin
+		writeln('Ingrese el codigo de prenda');
+		readln(regm.cod_prenda);
+		if (regm.cod_prenda <> -1) then begin
+			writeln('Ingrese la descripcion');
+			readln(regm.descripcion);
+			writeln('Ingrese el color');
+			readln(regm.colores);
+			writeln('Ingrese el tipo de prenda');
+			readln(regm.tipo_prenda);
+			writeln('Ingrese el stock');
+			readln(regm.stock);
+			writeln('Ingrese el precio unitario');
+			readln(regm.precio_unitario);
+		end;
+	end;
+	
+	procedure leerDetalle(var regd:registroDetalle);
+	begin
+		writeln('Ingrese el codigo de prenda');
+		readln(regd.cod_prenda);
+	end;	
+	
+	procedure crearMaestro(var mae:maestro);
+	var
+		regm:registroMaestro;
+	begin
+		assign(mae,'maestro');
+		rewrite(mae);
+		leerMaestro(regm);
+		while (regm.cod_prenda <> -1) do begin
+			write(mae,regm);
+			leerMaestro(regm);
+		end;
+		close(mae);
+	end;
+	
+	procedure crearDetalle(var det:detalle);
+	var
+		regd:registroDetalle;
+	begin
+		assign(det,'detalle');
+		rewrite(det);
+		leerDetalle(regd);
+		while (regd.cod_prenda <> -1) do begin
+			write(det,regd);
+			leerDetalle(regd);
+		end;
+		close(det);
+	end;
 	
 	procedure bajaLogica(var mae:maestro; var det:detalle);	
 	var
 		regm:registroMaestro;
 		regd:registroDetalle;
 	begin
-		reset(mae)
+		reset(mae);
 		reset(det);
 		while (not eof(det)) do begin
-			seek(det,0);
 			read(det,regd);
 		    read(mae,regm);
-			while (regd.codigo <> regm.codigo) and (not eof(det)) do
+			while (regd.cod_prenda <> regm.cod_prenda) and (not eof(det)) do
 				read(det,regd);
-			if (regd.codigo = regm.codigo) then begin
-				regm.stock := regm.stock*(-1);
+			if (regd.cod_prenda = regm.cod_prenda) then begin
+				regm.stock := regm.stock*-1;
+				seek(mae,filepos(mae)-1);
 				write(mae,regm);
-				read(mae,regm);
+			seek(det,0);
 			end;
 		end;
-		close(mae)
+		close(mae);
 		close(det);
 	end;
 	
@@ -62,8 +114,10 @@ type
 		reset(mae);
 		while (not eof(mae)) do begin
 			read(mae,regm);
-			if (regm.stock >= 0) then
+			if (regm.stock >= 0) then begin
+				writeln(regm.stock);
 				write(nuevoMae,regm);
+			end;
 		end;
 		close(mae);
 		close(nuevoMae);
@@ -71,12 +125,26 @@ type
 		rename(nuevoMae,'maestro');
 	end;
 	
+	procedure informarMaestro(var mae:maestro);
+	var
+		regm:registroMaestro;
+	begin
+		reset(mae);
+		writeln('Codigos de prenda del archivo maestro:');
+		while (not eof(mae)) do begin
+			read(mae,regm);
+			writeln('Codigo prenda: ',regm.cod_prenda);
+		end;
+		close(mae);
+	end;
+	
 var
 	mae,nuevoMae:maestro;
 	det:detalle;
 begin
-	importarMaestro();
-	importarDetalle(det);
+	crearMaestro(mae); // se dispone
+	crearDetalle(det); // se dispone
 	bajaLogica(mae,det);
 	compactarArchivo(mae,nuevoMae);
+	informarMaestro(nuevoMae);
 end.
