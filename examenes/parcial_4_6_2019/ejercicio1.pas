281fed9b662b439730614750d78057c9a3a91548
Valentin Capelli <valentin.capelli@hotmail.com>
2024-05-23 18:11:41 -0300

    Parciales

---
+++ examenes/parcial_4_6_2019/ejercicio1.pas
@@ -0,0 +1,180 @@
+{1. Archivos Secuenciales
+Una empresa que comercializa fármacos recibe de cada una de sus 30 sucursales un resumen mensual de las ventas y 
+desea analizar la información para la toma de futuras decisiones. El formato de los archivos que recibe la empresa
+ es: cod_farmaco, nombre, fecha, cantidad_vendida y forma_pago (campo String indicando contado o tarjeta).
+Los archivos de ventas están ordenados por: cod_farmaco y fecha.
+
+Cada sucursal puede vender cero, uno o más veces determinado fármaco el mismo día, y la forma de pago podría variar
+en cada venta. Realizar los siguientes procedimientos:
+a) Recibe los 30 archivos de ventas e informa por pantalla el fármaco con mayor cantidad_vendida.
+b) Recibe los 30 archivos de ventas y guarda en un archivo de texto un resumen de ventas por fecha y fármaco con 
+el siguiente formato: cod_farmaco, nombre, fecha, cantidad_total_vendida. (el archivo de texto deberá estar 
+organizado de manera tal que al tener que utilizarlo pueda recorrer el archivo realizando la menor cantidad de lecturas posibles). 
+Nota: en el archivo de texto por fecha, cada fármaco aparecerá a lo sumo una vez. 
+Además de escribir cada procedimiento deberá declarar las estructuras de datos utilizadas.}
+
+program ejercicio1;
+const
+    valoralto = 9999;
+    sucursales = 2;
+type
+    rango_sucursales = 1..sucursales;
+    str30 = string[30];
+    str20 = string[20];
+    registroDetalle = record
+        cod:integer;
+        nombre:str30;
+        fecha:str20;
+        cantidad_vendida:integer;
+        forma_pago:str30;
+    end;
+
+    detalle = file of registroDetalle;
+    vector = array[rango_sucursales] of detalle;
+    vectorRegistroDetalle = array[rango_sucursales] of registroDetalle;
+
+    registroMaestro = record
+        cod:integer;
+        nombre:str30;
+        fecha:str20;
+        cantidad_vendida:integer;
+    end;
+    maestro = file of registroMaestro;
+
+    procedure leer(var det:detalle; var regd:registroDetalle);
+    begin
+        if (not eof(det)) then
+            read(det,regd)
+        else
+            regd.cod:= valoralto;
+    end;
+
+    procedure importarDetalle(var det:detalle);
+    var
+        txt:text;
+        regd:registroDetalle;
+        nombre:string;
+    begin
+        writeln('Ingrese el nombre del archivo binario detalle');
+        readln(nombre);
+        assign(det,nombre);     
+        rewrite(det);
+    
+        writeln('Ingrese el nombre del archivo txt detalle');
+        readln(nombre);
+        assign(txt,nombre);
+        reset(txt);
+    
+        while (not eof(txt)) do begin
+            readln(txt, regd.cod, regd.cantidad_vendida, regd.nombre);
+            readln(txt, regd.fecha);
+            readln(txt, regd.forma_pago);
+            write(det, regd);
+        end;
+        writeln('La importacion del archivo detalle se realizo con exito');
+        close(det);
+        close(txt);
+    end;
+
+    procedure cargarDetalles(var v:vector);
+    var
+        i:integer;
+    begin
+        for i:= 1 to sucursales do
+            importarDetalle(v[i]);
+    end;
+
+    procedure minimo(var v:vector; var vrd:vectorRegistroDetalle;  var min:registroDetalle);
+    var
+        i,pos:integer;
+    begin
+        min.cod:= valoralto;
+        min.fecha:= 'ZZZZ';
+        for i:= 1 to sucursales do begin
+            if (vrd[i].cod < min.cod) or ((vrd[i].cod = min.cod) and (vrd[i].fecha < min.fecha)) then begin
+                min:= vrd[i];
+                pos:= i;
+            end;
+        end;
+        if (min.cod <> valoralto) then
+            leer(v[pos],vrd[pos]);
+    end;
+
+    procedure crearMaestro(var mae:maestro; v:vector);
+    var
+        vrd:vectorRegistroDetalle;
+        min:registroDetalle;
+        actual:registroMaestro;
+        i:integer;
+    begin
+        assign(mae,'Maestro');
+        rewrite(mae);
+        for i:= 1 to sucursales do begin
+            reset(v[i]);
+            leer(v[i],vrd[i]);
+        end;
+        minimo(v,vrd,min);
+        while (min.cod <> valoralto) do begin
+            actual.nombre:= min.nombre; // me guardo el campo nombre en actual, para despues poder escribir en el maestro
+            actual.cod:= min.cod;
+            while (actual.cod = min.cod) do begin
+                actual.cantidad_vendida:= 0;
+                actual.fecha:= min.fecha;
+                while (actual.cod = min.cod) and (actual.fecha = min.fecha) do begin
+                    actual.cantidad_vendida:= actual.cantidad_vendida + min.cantidad_vendida;
+                    minimo(v,vrd,min);
+                end;
+                write(mae,actual);
+            end;
+        end;
+        writeln('Se creo el maestro');
+        close(mae);
+    end;
+
+    procedure maximoFarmaco(v:vector;  var mae:maestro);
+    var
+		max,maxFarmaco:integer;
+		regm:registroMaestro;
+    begin
+        crearMaestro(mae,v); // mergea los 30 detalles en un maestro
+        reset(mae);
+        max:= -1;
+        read(mae,regm);
+        while (not eof(mae)) do begin
+            if (regm.cantidad_vendida > max) then begin
+                max:= regm.cantidad_vendida;
+                maxFarmaco:= regm.cod;
+            end;
+            read(mae,regm);
+        end;
+        close(mae);
+        writeln('El maximo es ', maxFarmaco);
+    end;
+
+    procedure listarArchivo(var mae:maestro; v:vector);
+    var
+        txt:text;
+        regm:registroMaestro;
+    begin
+        // crearMaestro(mae,v);
+        assign(txt,'maestro.txt');
+        rewrite(txt);
+        reset(mae);
+        read(mae,regm);
+        while (not eof(mae)) do begin
+            writeln(txt, regm.cod,' ', regm.cantidad_vendida,' ', regm.nombre);
+            writeln(txt, regm.fecha);
+            read(mae,regm);
+        end;
+        close(mae);
+        close(txt);
+    end;
+
+var
+    v:vector;
+    mae:maestro;
+begin
+    cargarDetalles(v);
+    maximoFarmaco(v,mae);
+    listarArchivo(mae,v);
+end.
---
+++ examenes/parcial_fod_2023_1rafecha_tema2/ejercicio1.pas
@@ -0,0 +1,83 @@
+{1. Archivos Secuenciales
+Suponga que tiene un archivo con información referente a los empleados que trabajan en una multinacional.
+De cada empleado se conoce el dni (único), nombre, apellido, edad, domicilio y fecha de nacimiento.
+Se solicita hacer el mantenimiento de este archivo utilizando la técnica de reutilización de espacio 
+llamada lista invertida.
+
+Declare las estructuras de datos necesarias e implemente los siguientes módulos:
+Agregar empleado: solicita al usuario que ingrese los datos del empleado y lo agrega al archivo sólo si
+el dni ingresado no existe. Suponga que existe una función llamada existeEmpleado que recibe un dni y un 
+archivo y devuelve verdadero si el dni existe en el archivo o falso en caso contrario. La función 
+existe Empleado no debe implementarla. Si el empleado ya existe, debe informarlo en pantalla.
+Quitar empleado: solicita al usuario que ingrese un dni y lo elimina del archivo solo si este dni existe.
+Debe utilizar la función existeEmpleado. En caso de que el empleado no exista debe informarse en pantalla.
+Nota: Los módulos que debe implementar deberán guardar en memoria secundaria todo cambio que se produzca en 
+el archivo.}
+
+program parcial2023;
+type
+    registroEmpleado = record
+        dni:integer;
+        nombre:string[30];
+        apellido:string[30];
+        edad:integer;
+        domicilio:string[30];
+        fecha:string[20];
+    end;
+    archivoEmpleados = file of registroEmpleado;
+
+    procedure agregarEmpleado(var arch:archivoEmpleados);
+    var
+        e:registroEmpleado;
+    begin
+        ingresarEmpleado(e);
+        if (not existeEmpleado(arch,e.dni)) then
+            reset(arch);
+            read(arch,aux);
+            if (aux.codigo < 0) then begin
+                seek(arch, aux.codigo*(-1));
+                read(arch,aux);
+                seek(arch, filepos(arch)-1);
+                write(arch,n);
+                seek(arch,0);
+                write(arch,aux); 
+            end
+            else begin
+                seek(arch,filesize(arch));
+                write(arch,n);
+            end;
+            close(arch);
+            writeln('El empleado fue registrado.');
+        end
+        else writeln('El empleado ya existe.');
+    end;
+
+    procedure quitarEmpleado(var arch:archivoEmpleados);
+    var
+        dni:integer;
+    begin
+        ingreseDNI(dni);
+        if (existeEmpleado(arch,dni)) then begin
+            reset(arch);
+            read(arch, aux);
+            oldHead:= aux.dni;
+            while (not eof(arch)) and (aux.dni <> dni) do
+                read(arch,aux);
+            if (aux.dni = dni) then begin
+                aux.dni:= oldHead;
+                seek(arch,filepos(arch)-1);
+                newHead:= (filepos(arch)*-1)
+                write(arch,aux);
+                seek(arch,0);
+                aux.codigo:= newHead;
+                write(arch,n);
+                writeln('El empleado fue borrado.');
+            end;
+            close(arch);
+        end
+        else writeln('El empleado no existe.');
+    end;
+
+var
+begin
+end.
